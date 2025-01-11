dd679dcb1d23243b20e49432f772d6e66e3b0dd7
mvamsi259 <m.vamsi259@gmail.com>
2025-01-11 16:44:20 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "region" {
+    description = "Location"
+    type = string
+    default = ""
+}
+
+variable "access_key" {
+    description = "Key value"
+    type = string
+    default = ""    
+}
+
+variable "secret_key" {
+    description = "Key value"
+    type = string
+    default = ""    
+}
+
+variable "ami" {
+    description = "Image ami value"
+    type = string
+    default = ""    
+}
+
+variable "instance_type" {
+    description = "sIntance tier"
+    type = string
+    default = ""    
+}
