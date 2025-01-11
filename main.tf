455cee9706bde18400cd0d1f32df94bc1d9bc6d5
mvamsi259 <m.vamsi259@gmail.com>
2025-01-11 18:12:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
-provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
-
 resource "aws_instance" "ec2-example" {
     ami = var.ami
     instance_type = var.instance_type
 
     tags = {
-        Name = "Terraform EC2"
+        Name = "Terraform EC2 s3"
     }
 }
